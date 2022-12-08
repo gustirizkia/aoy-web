0ba0a2f45d4026dc2a24a1b75b42c0df1228728c
lembar.yokesen@gmail.com <lembar.yokesen@gmail.com>
2022-12-08 22:27:13 +0700

    .

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('level')->unsigned()->nullable();
+            $table->integer('ref_id')->unsigned()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2022_12_03_013322_create_product_image_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_image', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->text('photo');
+            $table->tinyInteger('is_thumbnail')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_image');
+    }
+}
